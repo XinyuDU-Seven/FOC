792b68bac54a7964ca8d18eb07da7ec6c2ac6af5
du.xinyu <11918101@zju.edu.cn>
2026-06-18 11:30:34 +0800

    Increase speed loop gains for dynamic tracking

--- src/FOC_ExternalInterface_AI.c
+++ src/FOC_ExternalInterface_AI.c
@@ -419,9 +419,9 @@ void Foc_Init_AI(void){
 
   config.current_q_pid.out_min = -10;
 
-  config.speed_pid.kp = 0.005f;
+  config.speed_pid.kp = 0.0065f;
 
-  config.speed_pid.ki = 0.0005f;
+  config.speed_pid.ki = 0.0008f;
 
   config.speed_pid.kd = 0.0f;
 
